c8b689e57fe93a6a0017eb3d2f9a66bd147f6ce8
Khanallama <allama.khan@einzigtech.com>
2022-01-24 14:09:02 +0530

    Registering admin manually through fixture after installing fixture package

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setName('admin');
+        $user->setEmail('admin@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $password = $this->hasher->hashPassword($user, 'pass_1234');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
